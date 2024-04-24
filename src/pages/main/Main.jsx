582e56b55069c966a3bd9f3b07889caf9eaa3e47
JUSTINE IMASIKU <justineimasiku21@gmail.com>
2024-04-24 19:45:52 +0000

    update the generate story function to allow regeneration of story

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -24,6 +24,7 @@ const Main = () => {
 
   const generateStory = (e) => {
     e.preventDefault();
+    setStatus("unprocessed");
     if (!selectedImage) {
       alert("Please select an image first");
       
